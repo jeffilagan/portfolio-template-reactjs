37f0569b99501df5567e7e5a54fa2525e6dfb0c3
earn <earn.jilagan@gmail.com>
2026-04-19 16:24:25 +0800

    Fix theme to dark mode

--- src/components/layout/app.header.tsx
+++ src/components/layout/app.header.tsx
@@ -1,24 +1,12 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { MdOutlineLightMode, MdNightlight } from "react-icons/md";
 import { Link, NavLink } from 'react-router-dom';
-import { useCurrentApp } from '../context/app.context';
-
-type ThemeContextType = "dark" | "light";
 
 function AppHeader() {
-    const { theme, setTheme } = useCurrentApp();
-
-    const handleMode = (mode: ThemeContextType) => {
-        localStorage.setItem("theme", mode);
-        document.documentElement.setAttribute('data-bs-theme', mode);
-        setTheme(mode);
-    }
-
     return (
         <Navbar
-            data-bs-theme={theme}
+            data-bs-theme="dark"
             expand="lg"
             className="bg-body-tertiary"
             style={{ zIndex: 1, fontWeight: "600", backgroundColor: "#1c1c22", borderBottom: "1px solid #2d2d35" }}
@@ -37,21 +25,6 @@ function AppHeader() {
                         <NavLink className="nav-link" to="/resume" style={{ color: "#fff" }}>Resume</NavLink>
                         <NavLink className="nav-link" to="/work" style={{ color: "#fff" }}>Work</NavLink>
                         <NavLink className="nav-link" to="/contact" style={{ color: "#fff" }}>Contact</NavLink>
-                    </Nav>
-                    <Nav className="ms-auto">
-                        <div className='nav-link' style={{ cursor: "pointer", color: "#fff" }}>
-                            {theme === "light" ?
-                                <MdOutlineLightMode
-                                    onClick={() => handleMode("dark")}
-                                    style={{ fontSize: 20 }}
-                                />
-                                :
-                                <MdNightlight
-                                    onClick={() => handleMode("light")}
-                                    style={{ fontSize: 20 }}
-                                />
-                            }
-                        </div>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
